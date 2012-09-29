54ec5668a378112e1007b3ad22a5b6dd42653046
ahmdrefat <ahmd.refat.twfik@gmail.com>
2012-09-29 20:33:28 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisBook::Application.config.secret_token = '07d59c4a6eb4bb0604d24b9e8a3702c5326c5de2e818ce8756007b35c85d4ca3c58a0ec8d74eaf29a68070753418ca2e938917bcfd89c882dd96be3f3d330a68'
